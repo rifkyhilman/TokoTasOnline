2389e5cbea8ac1ee8d20131abe76d18d3f2bfe02
rifkyhillman26 <rifkyhilman265@gmail.com>
2021-05-29 23:20:48 +0700

    edit Gambar Cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -11,11 +11,10 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info" >
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1544816155-12df9643f363?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1619646176605-b7417fb53b1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Vans Tinggi Hitam</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
                                             IDR 649.900,00
                                         </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
@@ -28,11 +27,10 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1546938576-6e6a64f317cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1525966222134-fcfa99b8ae77?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=343&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Vans Merah</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
                                             IDR 649.900,00
                                         </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
@@ -45,11 +43,10 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1548863227-3af567fc3b27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1548863227-3af567fc3b27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">ToteBack Putih</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
                                             IDR 649.900,00
                                         </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
@@ -62,11 +59,10 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1526735592105-692eb4df7094?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1374&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1618860104306-afcff5629f30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Sepatu Sekolah</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
                                             IDR 649.900,00
                                         </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
@@ -81,13 +77,12 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1553062407-98eeb64c6a62?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1594576722512-582bcd46fba3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Jam Tangan Pria</Card.Title>
                                         <Card.Text>
                                             IDR 99.900,00
-                                            IDR 649.900,00
-                                </Card.Text>
+                                        </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
                                     </Card.Body>
                                 </Card>
@@ -98,13 +93,12 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1557669246-38f5ebeb001c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Sepatu High Healls Wanita</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
                                             IDR 649.900,00
-                                </Card.Text>
+                                        </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
                                     </Card.Body>
                                 </Card>
@@ -115,13 +109,12 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1584917865442-de89df76afd3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Tas Wanita Merah</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
                                             IDR 649.900,00
-                                </Card.Text>
+                                        </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
                                     </Card.Body>
                                 </Card>
@@ -132,13 +125,12 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1590874103328-eac38a683ce7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=377&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1590874103328-eac38a683ce7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=377&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Tas Wanita Oren</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
                                             IDR 649.900,00
-                                </Card.Text>
+                                        </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
                                     </Card.Body>
                                 </Card>
@@ -151,29 +143,11 @@ class Cards extends React.Component {
                         <div>
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1572196284554-4e321b0e7e0b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1572196284554-4e321b0e7e0b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">ToteBack Pria / Wanita</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
-                                            IDR 649.900,00
-                                </Card.Text>
-                                        <Button variant="outline-light">Check Out</Button>
-                                    </Card.Body>
-                                </Card>
-                            </Link>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="LastCard">
-                            <Link to="/Jualan">
-                                <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1581605405669-fcdf81165afa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80" />
-                                    <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
-                                        <Card.Text>
-                                            IDR 99.900,00
-                                            IDR 649.900,00
+                                            IDR 99.900
                                         </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
                                     </Card.Body>
@@ -185,12 +159,11 @@ class Cards extends React.Component {
                         <div className="LastCard">
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1600857062241-98e5dba7f214?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=429&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1581605405669-fcdf81165afa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Tas Ransel Pria</Card.Title>
                                         <Card.Text>
-                                            IDR 99.900,00
-                                            IDR 649.900,00
+                                            IDR 99.900
                                         </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
                                     </Card.Body>
@@ -202,12 +175,27 @@ class Cards extends React.Component {
                         <div className="LastCard">
                             <Link to="/Jualan">
                                 <Card className="card text-center bg-info">
-                                    <Card.Img variant="top" src="https://images.unsplash.com/photo-1571273260782-bab4699dde20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=334&q=80" />
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1600857062241-98e5dba7f214?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=429&q=80" />
                                     <Card.Body className="text-white">
-                                        <Card.Title class="font-weight-bold"> LITE - NO. 30 </Card.Title>
+                                        <Card.Title class="font-weight-bold">Tas Wanita</Card.Title>
                                         <Card.Text>
                                             IDR 99.900,00
-                                            IDR 649.900,00
+                                        </Card.Text>
+                                        <Button variant="outline-light">Check Out</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Link>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="LastCard">
+                            <Link to="/Jualan">
+                                <Card className="card text-center bg-info">
+                                    <Card.Img className="imgCards" variant="top" src="https://images.unsplash.com/photo-1612504722323-28ae76e4ac47?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80" />
+                                    <Card.Body className="text-white">
+                                        <Card.Title class="font-weight-bold">Jam Tangan</Card.Title>
+                                        <Card.Text>
+                                            IDR 99.900,00
                                          </Card.Text>
                                         <Button variant="outline-light">Check Out</Button>
                                     </Card.Body>
